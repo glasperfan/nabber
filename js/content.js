9585467effd3fb10abfe5231fbb282b4ca24e15b
Hugh Zabriskie <hzabriskie@college.harvard.edu>
2015-12-25 14:49:01 -0600

    able to find pdfs

--- js/content.js
+++ js/content.js
@@ -1,24 +1,51 @@
 // content.js
 
 // listen for action
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  	// iterate over the images
-  	var images = document.getElementsByTagName('img');
-  	for (var i in images) {
-  		downloadImage(images[i]);
-  	}
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   	// iterate over the images
+//   	var images = document.getElementsByTagName('img');
+//   	for (var i in images) {
+//   		downloadImage(images[i]);
+//   	}
+// });
+
+// on page load, make an index of all of the images and documents on the page, and send that to the popup
+$(document).ready(function () {
+	
+	// collect images
+	var images = document.getElementsByTagName('img');
+	var docs = collectPDFs();
+
 });
 
-// takes an image element and returns the filename
-function getName(img) {
-	return img.src.replace(/^.*[\\\/]/, '');	
+// takes an image or link element and returns the filename
+function getName(el) {
+	var slashpos = el.lastIndexOf("/");
+	var fileName = el.substr(slashpos + 1);
+	if (slashpos === -1 || fileName.length < 1) {
+		return "Untitled";
+	}
+	return fileName;
+}
+
+// find all links to pdfs on a page
+function collectPDFs() {
+	var links = document.getElementsByTagName('a');
+	var documents = [];
+	for (var i=0; i < links.length; i++) {
+		var el = links[i];
+		if (el.href.indexOf(".pdf") != -1) {
+			documents.push(getName(el.href));
+		}
+	}
+	console.log(documents);
 }
 
 // takes an image element and downloads it
 function downloadImage(img) {
 	var a = document.createElement('a');
 	a.href = img.src;
-	a.download = getName(img);
+	a.download = getName(img.src);
 	document.body.appendChild(a);
 	a.click();
 	document.body.removeChild(a);
